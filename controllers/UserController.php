47b90369f2dd6a15989e22500579dff175ac6f3f
KBogdan <cs300680kbp@gmail.com>
2017-06-29 17:04:15 +0300

    registration new user, p.1

--- controllers/UserController.php
+++ controllers/UserController.php
@@ -5,3 +5,44 @@
  *   
  */
 
+/**
+ * підключаємо моделі
+ */
+include_once '../models/CategoriesModel.php';
+include_once '../models/UsersModel.php';
+
+
+/**
+ * AJAX реєстрація користувача
+ * Ініціалізація сесійної змінної ($_SESSION['user'])
+ * 
+ * @return json масив даних нового користувача
+ */
+function registerAction(){
+    
+    $email = isset($_REQUEST['email']) ? $_REQUEST['email'] : null;
+    $email = trim($email);
+    
+    $pwd1 = isset($_REQUEST['pwd1']) ? $_REQUEST['pwd1'] : null;
+    $pwd2 = isset($_REQUEST['pwd2']) ? $_REQUEST['pwd2'] : null;
+    
+    $phone = isset($_REQUEST['phone']) ? $_REQUEST['phone'] : null;
+    $adress = isset($_REQUEST['adress']) ? $_REQUEST['adress'] : null;
+    $name = isset($_REQUEST['name']) ? $_REQUEST['name'] : null;
+    $name = trim($name);
+    
+    $resData = null;
+    $resData = checkRegisterParams($email, $pwd1, $pwd2);
+    
+    //перевіряємо чи є вже зареєстрований користувач
+    if (! $resData && checkUserEmail($email)){
+        $resData['success'] = false;
+        $resData['message'] = "Користувач з таким email ('{$email}') вже зареєстрований";
+    }
+    
+    if (! $resData) {
+        $pwdMD5 = md5($pwd1);
+        
+        $userData = registerNewUser($email, $pwdMD5, $name, $phone, $adress);
+    }
+}
--- models/UsersModel.php
+++ models/UsersModel.php
@@ -5,3 +5,92 @@
  *  
  */
 
+/**
+ * Реєстрація нового користувача
+ * 
+ * @param string $email пошта
+ * @param string $pwdMD5 пароль, зашифрований в MD5
+ * @param string $name ім'я користувача
+ * @param string $phone телефон
+ * @param string $adress адреса користувача
+ * @return array масив данних нового користувача
+ */
+function registerNewUser($email, $pwdMD5, $name, $phone, $adress){
+    
+    $email = htmlspecialchars(mysql_real_escape_string($email));
+    $name = htmlspecialchars(mysql_real_escape_string($name));
+    $phone = htmlspecialchars(mysql_real_escape_string($phone));
+    $adress = htmlspecialchars(mysql_real_escape_string($adress));
+    
+    $sql = "INSERT INTO users (`email`, `pwd`, `name`, `phone`, `adress`)"
+            . "VALUES('{$email}', '{$pwdMD5}', '{$name}', '{$phone}', '{$adress}')";
+            
+            $rs = mysql_query($sql);
+
+            if ($rs){
+                $sql = "SELECT * FROM users WHERE (`email` = '{$email}' and `pwd` = '{$pwdMD5}') LIMIT 1";
+                
+                $rs = mysql_query($sql);
+                $rs = createSmartyRsArray($rs);
+                
+                if (isset($rs[0])){
+                    $rs['success'] = 1;
+                  } else {
+                      $rs['success'] = 0;
+                  }
+            } else {
+                $rs['success'] = 0;
+            }
+            return $rs;
+ }
+
+ /**
+  * Перевірка параметрів для реєстрації користувача
+  * 
+  * @param string $email email
+  * @param string $pwd1 пароль
+  * @param string $pwd2 повторення пароля
+  * @return array результат
+  */
+ function checkRegisterParams($email, $pwd1, $pwd2){
+     $res = null;
+     
+     if (! $email){
+         $res['success'] = false;
+         $res['message'] = 'Введіть Ваш email';
+     }
+     
+     if (! $pwd1){
+         $res['success'] = false;
+         $res['message'] = 'Введіть Ваш пароль';
+     }
+     
+     if (! $pwd2){
+         $res['success'] = false;
+         $res['message'] = 'Повторно введіть Ваш пароль';
+     }
+     
+     if ($pwd1 != $pwd2){
+         $res['success'] = false;
+         $res['message'] = 'Введені Вами паролі не співпадають';
+     }
+     
+     return $res;
+ }
+ 
+ /**
+  * Перевірка email (чи є email-адреса в БД)
+  * 
+  * @param string $email
+  * @return array масив - строка з таблиці users або пустий масив
+  */
+ function checkUserEmail($email){
+     
+     $email = mysql_real_escape_string($email);
+     $sql = "SELECT id FROM users WHERE email = '{$email}'";
+     
+     $rs = mysql_query($sql);
+     $rs = createSmartyRsArray($rs);
+     
+     return $rs;
+ }
